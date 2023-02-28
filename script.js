bcdd99fe0428739612c4f50d2e2cf158932f0cd0
Kip Dunlap <kipling.dunlap@gmail.com>
2023-02-28 15:57:30 -0500

    auto copy board decision, log response

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ form.addEventListener('submit', async function (event) {
   // const form = document.getElementById('form');
   let Zquery = document.getElementById('Zquery').value;
   let status = document.getElementById('status');
+  let decision = document.getElementById('decision');
   let link = document.getElementById('link');
   let Zstatus = '';
   let ordLink = '';
+  // let decisionSpace = '';
   await fetch(`https://gis.atlantaga.gov/dpcd/rest/services/LandUsePlanning/LandUsePlanning/MapServer/10/query?where=DOCKET_NO%3D'${Zquery}'&outFields=ORDHYPERLINK,%20STATUSTYPE&returnGeometry=false&returnTrueCurves=false&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&returnDistinctValues=false&returnExtentOnly=false&f=pjson`)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      // console.log(data);
+      console.log(data.features[0].attributes);
       ordLink = data.features[0].attributes.ORDHYPERLINK || 'No Data';
       Zstatus = data.features[0].attributes.STATUSTYPE;
       return { ordLink, Zstatus }
@@ -26,10 +28,12 @@ form.addEventListener('submit', async function (event) {
     case 'Complete':
       status.innerText = Zstatus;
       status.style.color = 'green';
+      decision.innerText = 'ZRB - Approval';
       link.innerText = Zquery;
       link.setAttribute('href', ordLink);
       link.setAttribute('target', '_blank');
-      saveData();
+      copyDecision();
+      // saveData();
       break;
     case 'Correction':
       status.innerText = Zstatus;
@@ -37,15 +41,17 @@ form.addEventListener('submit', async function (event) {
       link.innerText = Zquery;
       link.setAttribute('href', ordLink);
       link.setAttribute('target', '_blank');
-      saveData();
+      // saveData();
       break;
     case 'Denied':
       status.innerText = Zstatus;
       status.style.color = 'red';
+      decision.innerText = 'ZRB - Denial';
       link.innerText = Zquery;
       link.setAttribute('href', ordLink);
       link.setAttribute('target', '_blank');
-      saveData();
+      copyDecision();
+      // saveData();
       break;
     case 'Filed':
       status.innerText = Zstatus;
@@ -53,7 +59,7 @@ form.addEventListener('submit', async function (event) {
       link.innerText = Zquery;
       link.setAttribute('href', ordLink);
       link.setAttribute('target', '_blank');
-      saveData();
+      // saveData();
       break;
     case 'Pending':
       status.innerText = Zstatus;
@@ -61,7 +67,7 @@ form.addEventListener('submit', async function (event) {
       link.innerText = Zquery;
       // link.setAttribute('href', ordLink);
       // link.setAttribute('target', '_blank');
-      saveData();
+      // saveData();
       break;
     case 'Reserved':
       status.innerText = Zstatus;
@@ -69,7 +75,7 @@ form.addEventListener('submit', async function (event) {
       link.innerText = Zquery;
       link.setAttribute('href', ordLink);
       link.setAttribute('target', '_blank');
-      saveData();
+      // saveData();
       break;
     default:
       status.innerText = Zstatus || 'No Data';
@@ -82,16 +88,49 @@ form.addEventListener('submit', async function (event) {
   form.reset();
 });
 
-function saveData() {
-  let Zquery = document.getElementById('Zquery').value;
-  let status = document.getElementById('status').innerText;
-  let link = ordLink;
-  let data = {
-    Zquery,
-    status,
-    link
-  };
-  let dataArray = [];
-  dataArray.push(data);
-  localStorage.setItem('data', JSON.stringify(dataArray));
-};
+// function saveData() {
+//   let Zquery = document.getElementById('Zquery').value;
+//   let status = document.getElementById('status').innerText;
+//   let link = ordLink;
+//   let data = {
+//     Zquery,
+//     status,
+//     link
+//   };
+//   let dataArray = [];
+//   dataArray.push(data);
+//   localStorage.setItem('data', JSON.stringify(dataArray));
+// };
+
+// const ZqueryInput = document.getElementById("Zquery");
+
+// ZqueryInput.oninput = (e) => {
+//   e.target.value = formatZquery(e.target.value);
+//   console.log('Input is inputting...')
+// };
+
+// //  format the Zquery input
+// function formatZquery(zQueryString) {
+//   try {
+//     var match = zQueryString.match(/(^[vz]{1})\-([\d]{2})\-([\d]{1,3})$/gi);
+//     return [
+//       match[0].slice(0, 1).toUpperCase(),
+//       match[1].slice(1, 3),
+//       match[2].slice(4, 7)
+//     ].join("-");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// copy decision to clipboard
+function copyDecision() {
+  let decision = document.getElementById('decision');
+  let range = document.createRange();
+  range.selectNode(decision);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  navigator.clipboard.writeText(decision.innerText);
+  window.getSelection().removeAllRanges();
+  console.log(`'${decision.innerText}' copied to clipboard`);
+}
